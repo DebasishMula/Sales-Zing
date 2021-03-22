f2bd550d4a0ce01aaa85dffc8811eab0522443a0
Debashish <Debashish@circuitworld.in>
2021-03-22 11:13:27 +0530

    mshj

--- app/src/main/java/com/example/testproject2/activities/Pos.java
+++ app/src/main/java/com/example/testproject2/activities/Pos.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
